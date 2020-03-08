9bf738aeb227b3719165079c93dd524add561d55
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-08 13:41:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,12 +1,12 @@
 require 'pry'
 def key_for_min_value(name_hash)
  name_hash= name_hash.to_a 
+   binding.pry
   i = 0 
  while i <name_hash.length 
   min = 0 
  name_hash.each do |number|
    num= number[1]  #the hash's value part of the array
-   binding.pry
    if num<min 
       min=num
       i+=1 
